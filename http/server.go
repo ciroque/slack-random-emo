686fce8b927415879c15924a2aee64365365d20b
swagner <swagner@marchex.com>
2019-10-21 11:42:26 -0700

    Should now post response in the channel

--- http/server.go
+++ http/server.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Sirupsen/logrus"
 	"math/rand"
@@ -28,7 +29,19 @@ func (server *Server) Run(settings *config.Settings, abortChannel chan<- string)
 func (server *Server) ServeRandomEmoji(writer http.ResponseWriter, request *http.Request) {
 	length := len(*server.Emos)
 	index := rand.Intn(length)
-	_, err := fmt.Fprintf(writer, ":%s:", (*server.Emos)[index].Name)
+
+	response := SlackEmojiResponse{
+		ResponseType: "in_channel",
+		Text:         (*server.Emos)[index].Name,
+		Attachments:  []map[string]string{},
+	}
+
+	bytes, err := json.Marshal(&response)
+	if err != nil {
+		server.Logger.Warnf("Error responding to request %#v", err)
+	}
+
+	_, err = fmt.Fprintf(writer, "%s", bytes)
 	if err != nil {
 		server.Logger.Warnf("Error responding to request %#v", err)
 	}
